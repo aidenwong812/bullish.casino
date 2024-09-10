dc283b4b6df7dae4e09f5f90e098a1178428a666
aidenwong812 <wongaiden812@gmail.com>
2024-09-11 02:32:24 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { TOKEN } = require('./config.js');
+const { handleUsername } = require('./game.js');
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on('message', async msg => {
+  try {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    
+    const { text } = msg;
+    const COMMANDS = text.toUpperCase();
+    
+    if (!text) return;
+
+    switch (COMMANDS) {
+      default:
+        handleUsername(bot, chatId, userId, text);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+})
